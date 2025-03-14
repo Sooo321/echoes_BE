b705e5b6a3c5354e3f6d196136bc2c3e4caceb55
Sooo321 <thguddlsp@naver.com>
2025-03-14 19:00:35 +0900

    Initial commit

---
+++ src/main/java/org/example/echoes_be/EchoesBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.echoes_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoesBeApplication.class, args);
+    }
+
+}
