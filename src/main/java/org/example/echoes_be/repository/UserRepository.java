5903f6d149e5c715a09e3a4689fd36b10faa3f0d
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-03-29 22:04:01 +0900

    사용자 회원가입 요청/ 로그인API

---
+++ src/main/java/org/example/echoes_be/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.example.echoes_be.repository;
+
+import org.example.echoes_be.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+
+
+    Optional<Users> findByEmail(String email);
+    Optional<Users> findByPassword(String password);
+}
