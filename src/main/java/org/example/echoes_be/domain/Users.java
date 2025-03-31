1b2409320fa16bd22841904bd5b88045cb79aa27
Sooo321 <thguddlsp@naver.com>
2025-03-31 12:55:08 +0900

    feat : 이메일 중복확인 및 인증메일 구현

--- src/main/java/org/example/echoes_be/domain/Users.java
+++ src/main/java/org/example/echoes_be/domain/Users.java
@@ -1,18 +1,31 @@
 package org.example.echoes_be.domain;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Table(name = "users")  // 'user'는 예약어라 'users'로 테이블명 지정
+@Data
+@Getter
+@Setter
+
 public class Users {
 
-    @Id //pk
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키(PK) 값을 자동으로 생성해주는 어노테이션
-
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nickname", nullable = false)
     private String nickname;
+
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
     private Long accessDate;
 
     public Users(){
@@ -20,6 +33,7 @@ public class Users {
         //클래스가 객체로 생성될 때 아무런 값을 전달하지 않아도 호출될 수 있는 생성자
         //@Autowired 로 주입할 때나 JPA 엔티티를 사용할 때 기본 생성자가 필요함.
     }
+
     public Users(String nickname, String email, String password, Long accessDate){
         this.nickname = nickname;
         this.email = email;
@@ -27,37 +41,6 @@ public class Users {
         this.accessDate = accessDate;
     }
 
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname){
-        this.nickname = nickname;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    public String getAccessDate(String accessDate){
-        return accessDate;
-    }
-
-    public void setAccessDate(Long accessDate){
-        this.accessDate = accessDate;
-    }
 }
 
 
